1a5b6999eecbcf8795c9397eb2984451695e9f09
Nome <ramon.pereira@prof.unibh.br>
2021-05-25 20:16:40 -0300

    FIrst Ionic app

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ActionSheetController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +8,41 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  constructor(public actionSheetControler: ActionSheetController) {}
 
+  async apresentarActionSheet() {
+    const actionSheet = await this.actionSheetControler.create({
+      header: 'Opções',
+      cssClass: 'classe',
+      buttons: [
+        {
+          text: "Editar",
+          icon: "pencil",
+          handler: () => {
+            alert("Clicado");
+          }
+        },
+        {
+          text: "Apagar",
+          icon: "trash",
+          role: "destructive",
+          handler: () => {
+            alert("Deletado");
+            /* escrever a função de apagar a imagem aqui */
+          }
+        },
+        {
+          text: "Cancelar",
+          icon: "close",
+          role: "cancel",
+          handler: () => {
+            alert("Cancelado");
+          }          
+        }]
+    })
+
+    await actionSheet.present();
+    const { role } = await actionSheet.onDidDismiss();
+    console.log("Cancelei e a role é ", role)
+  }
 }
